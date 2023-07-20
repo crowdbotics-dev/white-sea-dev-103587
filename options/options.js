3a6008e5a2016da822e0e2554c3785d6cb48874e
Crowdbotics <engineering@crowdbotics.com>
2023-07-20 23:20:47 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "white-sea-dev-103587",
+  url: "https://white-sea-dev-103587.botics.co",
+  api: "https://white-sea-dev-103587.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
